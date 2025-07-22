844a5b05944e373906a29ba9a948ac099e2b53e1
up1 <somkiat.p@gmail.com>
2025-07-22 14:12:48 +0700

    Hybrid search

--- rag/step_2.py
+++ rag/step_2.py
@@ -5,11 +5,15 @@
 
 import os
 import json
+from symtable import Function
+from types import FunctionType
 from typing import List, Dict
 from openai import OpenAI
 from pymilvus import FieldSchema, CollectionSchema, DataType
     
-from pymilvus import MilvusClient
+from pymilvus import (
+    MilvusClient, DataType, Function, FunctionType
+)
 
 def read_data(file_path: str) -> List[Dict]:
     # read json lines from a file
@@ -17,7 +21,6 @@ def read_data(file_path: str) -> List[Dict]:
     with open(file_path, 'r', encoding='utf-8') as file:
         for line in file:
             try:
-                print(f"Processing line: {line.strip()}")
                 item = json.loads(line.strip())
                 items.append(item)
             except json.JSONDecodeError as e:
@@ -50,11 +53,21 @@ def save_to_milvus(embedded_data: List[Dict], milvus_host: str, milvus_port: int
     # Define schema
     fields = [
         FieldSchema(name="id", dtype=DataType.INT64, is_primary=True),
-        FieldSchema(name="description", dtype=DataType.VARCHAR, max_length=500),
+        FieldSchema(name="description", dtype=DataType.VARCHAR, max_length=500, enable_analyzer=True),
+        FieldSchema(name="description_sparse", dtype=DataType.SPARSE_FLOAT_VECTOR),
+        FieldSchema(name="severity", dtype=DataType.VARCHAR, max_length=500),
         FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=len(embedded_data[0]['embedding']))
     ]
-    
     schema = CollectionSchema(fields)
+
+    # Prepare search for BM25
+    bm25_function = Function(
+        name="text_bm25_emb",
+        input_field_names=["description"],
+        output_field_names=["description_sparse"],
+        function_type=FunctionType.BM25,
+    )
+    schema.add_function(bm25_function)
 
     # Prepare index parameters
     index_params = client.prepare_index_params()
@@ -66,6 +79,14 @@ def save_to_milvus(embedded_data: List[Dict], milvus_host: str, milvus_port: int
         metric_type="COSINE"
     )
 
+    index_params.add_index(
+        field_name="description_sparse",
+        index_name="description_sparse_index",
+        index_type="SPARSE_INVERTED_INDEX",
+        metric_type="BM25",
+        params={"inverted_index_algo": "DAAT_MAXSCORE"}, # or "DAAT_WAND" or "TAAT_NAIVE"
+    )
+
     # Create collection
     client.create_collection(
         collection_name="bug_tracking",
@@ -75,13 +96,19 @@ def save_to_milvus(embedded_data: List[Dict], milvus_host: str, milvus_port: int
 
     # Insert data into collection
     datas = [
-        {"id": item['id'], "description": item['description'], "embedding": item['embedding']}
+        {"id": item['id'], 
+         "description": item['description'], 
+         "severity": item['severity']['name'],
+         "embedding": item['embedding']}
         for item in embedded_data
     ]
+    print(datas)
+    # Insert multiple items
     client.insert(
         collection_name="bug_tracking",
         data=datas
     )
+
 
 if __name__ == "__main__":
     # Read data from step_1_results.txt
--- rag/step_3.py
+++ rag/step_3.py
@@ -2,9 +2,57 @@
 import os
 from typing import List, Dict
 from openai import OpenAI
-from pymilvus import Collection
-from pymilvus import MilvusClient
+from pymilvus import MilvusClient, AnnSearchRequest, RRFRanker
 
+
+def hybrid_search_data(query: str, top_k: int = 5) -> List[Dict]:
+    client = OpenAI()
+    response = client.embeddings.create(input=query, model="text-embedding-3-small")
+    query_embedding = response.data[0].embedding
+
+    milvus_host = os.getenv('MILVUS_HOST', 'localhost')
+    milvus_port = int(os.getenv('MILVUS_PORT', 19530))
+    client = MilvusClient(
+        uri="http://" + milvus_host + ":" + str(milvus_port)
+    )
+
+    # Semantic search (embedding)
+    search_param_1 = {
+        "data": [query_embedding],
+        "anns_field": "embedding",
+        "param": {"nprobe": 10},
+        "limit": top_k,
+    }
+    request_1 = AnnSearchRequest(**search_param_1)
+
+    # Full-text search (sparse)
+    search_param_2 = {
+        "data": [query],
+        "anns_field": "description_sparse",
+        "param": {"drop_ratio_search": 0.2},
+        "limit": top_k,
+    }
+    request_2 = AnnSearchRequest(**search_param_2)
+
+    reqs = [request_1, request_2]
+
+    # Re-rannking
+    ranker = RRFRanker(100)
+
+    # Search data in Milvus
+    search_results = client.hybrid_search(
+        collection_name="bug_tracking",
+        reqs=reqs,
+        ranker=ranker,
+        limit=top_k,
+        output_fields=["id", "description", "severity"]
+    )
+
+    if not search_results or not search_results[0]:
+        print("No results found.")
+        return []
+
+    return [hit.entity for hit in search_results[0]]
 
 def search_data(query: str, top_k: int = 5) -> List[Dict]:
     client = OpenAI()
@@ -29,9 +77,9 @@ def search_data(query: str, top_k: int = 5) -> List[Dict]:
         collection_name="bug_tracking",
         data=[query_embedding],
         anns_field="embedding",
-        search_params=search_params,
+        # search_params=search_params,
         limit=top_k,
-        output_fields=["id", "description"],
+        output_fields=["id", "description", "severity"],
     )
     if not search_results or not search_results[0]:
         print("No results found.")
@@ -39,12 +87,11 @@ def search_data(query: str, top_k: int = 5) -> List[Dict]:
 
     return [hit.entity for hit in search_results[0]]
 
-
 if __name__ == "__main__":
     query = input("Enter a search query: ")
     top_k = int(input("Enter the number of results to return (default 5): ") or 5)
 
-    results = search_data(query, top_k)
+    results = hybrid_search_data(query, top_k)
 
     print(f"Found {len(results)} results:")
     for item in results:
